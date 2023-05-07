44e41b0efbfc1e02b1fba87b81b1dba57df13de6
Sadaf <sadaf@Sadafs-MacBook-Air-2.local>
2023-05-07 13:20:53 -0600

    add optimization

--- solver.py
+++ solver.py
@@ -47,7 +47,7 @@ def place_sketch(flow_dic):
         delta = 0.05
         num_of_regs = math.ceil(math.log(1/delta))
         sketch_size = num_of_regs * width * 4
-        m.addConstr(gp.quicksum(x_var[i, j] * sketch_size for j in range(len(list(flow_dic.keys())[i])) <= list(flow_dic.keys())[i][j].mem_available()))
+        m.addConstr(gp.quicksum(x_var[i, j] * sketch_size <= list(flow_dic.keys())[i][j].mem_available()) for j in range(len(list(flow_dic.keys())[i])))
     
     # Add the assignment constraints
     for i in range(num_of_ods):
