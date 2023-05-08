4bcbd0d060782e559ad39348f6b18866df1c1529
Sadaf <sadaf@Sadafs-MacBook-Air-2.local>
2023-05-08 15:48:53 -0600

    add optimization

--- solver.py
+++ solver.py
@@ -28,7 +28,7 @@ def place_sketch(flow_dic):
     #     else:
     #         print("cannot place sketch")
 
-    m = gp.Model()
+    m = gp.Model('Sketch Placement')
 
     # Add decision variables
     x_var = {}
@@ -47,14 +47,14 @@ def place_sketch(flow_dic):
         delta = 0.05
         num_of_regs = math.ceil(math.log(1/delta))
         sketch_size = num_of_regs * width * 4
-        m.addConstr(gp.quicksum(x_var[i, j] * sketch_size <= list(flow_dic.keys())[i][j].mem_available()) for j in range(len(list(flow_dic.keys())[i])))
+        m.addConstr(gp.quicksum(x_var[i, j] * sketch_size for j in range(len(list(flow_dic.keys())[i]))) <= list(flow_dic.keys())[i][j].mem_available() for j in range(len(list(flow_dic.keys())[i])))
     
     # Add the assignment constraints
     for i in range(num_of_ods):
         m.addConstr(gp.quicksum(x_var[i, j] for j in range(len(list(flow_dic.keys())[i]))) <= 1)
 
 
-    # Add uncertainty sets
+    # Add the robustness constraints
 
 
     # Optimize the model
