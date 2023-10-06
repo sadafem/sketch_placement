bc3fa1a16a9b1c50350e7d7237618ceabd71b0f9
sadafem <sadaf.erfan@gmail.com>
2023-10-06 16:01:44 -0600

    robust

--- optimization2.py
+++ optimization2.py
@@ -7,3 +7,10 @@ from scipy.stats import norm
 def check_feasibility(decision_vars):
     for v in decision_vars:
         print(v.key(),v.value())
+
+    m = gp.Model("fixed_variable_model")
+
+    x_fixed = m.addVar(vtype=gp.GRB.BINARY, lb=decision_vars, ub=decision_vars, name="x_fixed")
+
+
+    m.optimize()
